c0dccf71dc96cf975cf36a78aacb1fcc52ed4118
Sever Băneşiu <banesiu.sever@gmail.com>
2012-03-20 14:24:12 +0200

    Loaders to functions.

--- docs/loaders.rst
+++ docs/loaders.rst
@@ -3,32 +3,17 @@ Data Loaders
 
 .. py:module:: importtools.loaders
 
-There are also some format specific :py:class:`Loader` implementations
-available like :py:class:`ArgsLoader` that groups elements from a list in
-order to create :py:class:`~.importtools.importables.Importable` instances and
-a :py:class:`CSVLoader` that can be used to parse CSV files and construct
-:py:class:`~.importtools.importables.Importable` instances.
+There are also some format specific loaders available available like
+:py:function:`args_loader` that groups elements from a list in order to
+construct instances and a :py:function:`csv_loader` that can be used to parse
+CSV files.
 
-.. autoclass:: Loader
-   :members:
+.. function:: args_loader
 
-   .. automethod:: __iter__
+.. function:: csv_loader
 
-.. autoclass:: ArgsLoader
-   :show-inheritance:
-   :members:
-
-   .. automethod:: __iter__
-
-.. autoclass:: CSVLoader
-   :show-inheritance:
-   :members:
-
-   .. automethod:: __iter__
-
-The library provides a special *loader* that can be used to populate in
-parallel two ``DataSet`` instances in order to optimize a large import into
+The library provides a special *loader* function that can be used to populate
+in parallel two ``DataSet`` instances in order to optimize a large import into
 multiple smaller ones.
 
-.. autoclass:: ChunkedLoader
-   :members:
+.. autofunction:: chunked_loader
--- importtools/loaders.py
+++ importtools/loaders.py
@@ -1,78 +1,42 @@
 import csv
-import abc
 import heapq
 import itertools
 
 
-class Loader(object):
+def args_loader(source, factory, how_many=1):
     """
-    An :py:mod:`abc` that represents a data source used to generate
-    :py:class:`~.importtools.importables.Importable` instances.
+    A loader that yields values from a flattened list by grouping values
+    together and instantiating a factory.  This loader can be used to
+    prepopulate a datasets from command line arguments.
 
-    """
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def __iter__(self):
-        pass
-
-
-class ArgsLoader(Loader):
-    """
-    A :py:class:`Loader` implementation that gets prepopulated from a
-    flattened list by grouping values together and instantiating a factory.
-    The primary use of this implementation is to create a datasets from command
-    line arguments.
+    All ``None`` values returned by the factory are skipped.
 
     This is a short example where each two consecutive values are grouped
-    together and used to create tuple instances from a factory function.
+    together and used to create tuple instances from a factory function:
 
-    >>> from importtools.loaders import ArgsLoader
-    >>> from importtools.datasets import MemoryDataSet
-    >>> al = ArgsLoader(lambda x, y: tuple([x, y]), 2)
-    >>> al = al.bind(['STATE1', 'CL1', 'STATE2', 'CL2'])
-    >>> argsds = MemoryDataSet(al)
-    >>> argsds.get(('STATE1', 'CL1'))
-    ('STATE1', 'CL1')
-    >>> sorted(argsds)
-    [('STATE1', 'CL1'), ('STATE2', 'CL2')]
+    >>> from importtools.loaders import args_loader
+    >>> args = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+    >>> factory = lambda x, y: tuple([x, y])
+    >>> al = args_loader(args, factory, 2)
+    >>> sorted(al)
+    [('A1', 'A2'), ('B1', 'B2'), ('C1', 'C2')]
 
     """
-    def __init__(self, factory, how_many=1):
-        self.factory = factory
-        self.how_many = how_many
-        self.source = []
-
-    def bind(self, source):
-        """Bind a data source to this loader."""
-        self.source = source
-        return self
-
-    def _iter(self):
-        """Yield instances returned by the factory.
-
-        The factory is called with ``source`` values grouped together as
-        dictated by the value of ``how_many`` property.
-
-        """
-        source = self.source
-        assert len(source) % self.how_many == 0
-        # http://docs.python.org/library/itertools.html#recipes
-        arg_groups = zip(*[iter(source)] * self.how_many)
-        for arg_group in arg_groups:
-            value = self.factory(*arg_group)
-            if value is not None:
-                yield value
-
-    def __iter__(self):
-        return self._iter()
+    assert len(source) % how_many == 0
+    # http://docs.python.org/library/itertools.html#recipes
+    arg_groups = zip(*[iter(source)] * how_many)
+    for arg_group in arg_groups:
+        value = factory(*arg_group)
+        if value is not None:
+            yield value
 
 
-class CSVLoader(Loader):
+def csv_loader(source, factory, columns, has_header=True):
     """
-    A :py:class:`Loader` implementation that gets prepopulated from a ``CSV``
-    file and instantiate :py:class:`~.importtools.importables.Importable`
-    instances.
+    A loader that yields values from a ``CSV`` ``source`` file object by
+    calling the ``factory`` with the values specified in the ``columns``.
+
+    .. note:: The columns are 0 indexed.
 
     >>> import StringIO
     >>> source = StringIO.StringIO('''
@@ -80,98 +44,48 @@ class CSVLoader(Loader):
     ... R2C0,R2C1,R2C2,R2C3
     ... R3C0,R3C1,R3C2,R3C3
     ... '''.strip())
-    >>> from importtools.datasets import MemoryDataSet
+    >>> factory = lambda x, y: tuple([x, y,])
 
-    The file is parsed skipping the header and the ``DataSet`` is populated
-    with the correct values:
+    The file is parsed skipping the header:
 
-    >>> source.seek(0)
-    >>> csvl = CSVLoader(
-    ...     lambda x, y: tuple([x, y]),
-    ...     columns=[1,3],
-    ...     has_header=True)
-    >>> csvl = csvl.bind(source)
-    >>> csvds = MemoryDataSet(csvl)
-    >>> csvds.get((('R2C1', 'R2C3')))
-    ('R2C1', 'R2C3')
-    >>> csvds.get((('R3C1', 'R3C3')))
-    ('R3C1', 'R3C3')
-    >>> csvds.get((('R1C1', 'R1C3')), 'default')
-    'default'
+    >>> csvl = csv_loader(source, factory, columns=[1, 3], has_header=True)
+    >>> sorted(csvl)
+    [('R2C1', 'R2C3'), ('R3C1', 'R3C3')]
 
     The ``has_header`` flag can be used to avoid skipping the header:
 
     >>> source.seek(0)
-    >>> from importtools.loaders import CSVLoader
-    >>> csvl = CSVLoader(
-    ...     lambda x, y: tuple([x, y]),
-    ...     columns=[1,3],
-    ...     has_header=False)
-    >>> csvl = csvl.bind(source)
-    >>> csvds = MemoryDataSet(csvl)
-    >>> csvds.get((('R1C1', 'R1C3')))
-    ('R1C1', 'R1C3')
-    >>> csvds.get((('R2C1', 'R2C3')))
-    ('R2C1', 'R2C3')
-    >>> csvds.get((('R3C1', 'R3C3')))
-    ('R3C1', 'R3C3')
+    >>> csvl = csv_loader(source, factory, columns=[1, 3], has_header=False)
+    >>> sorted(csvl)
+    [('R1C1', 'R1C3'), ('R2C1', 'R2C3'), ('R3C1', 'R3C3')]
 
     By default, ``has_header`` flag is set and the number of column can vary:
 
     >>> source.seek(0)
-    >>> csvl = CSVLoader(
-    ...     lambda x, y, z: tuple([x, y, z]),
-    ...     columns=[1,2,3])
-    >>> csvl = csvl.bind(source)
-    >>> csvds = MemoryDataSet(csvl)
-    >>> csvds.get((('R2C1', 'R2C2', 'R2C3')))
-    ('R2C1', 'R2C2', 'R2C3')
-    >>> csvds.get((('R3C1', 'R3C2', 'R3C3')))
-    ('R3C1', 'R3C2', 'R3C3')
-    >>> csvds.get((('R1C1', 'R1C2', 'R1C3')), 'default')
-    'default'
+    >>> factory = lambda x, y, z: tuple([x, y, z])
+    >>> csvl = csv_loader(source, factory, columns=[1, 2, 3])
+    >>> sorted(csvl)
+    [('R2C1', 'R2C2', 'R2C3'), ('R3C1', 'R3C2', 'R3C3')]
 
     """
-
-    def __init__(self, factory, columns, has_header=True):
-        self.factory = factory
-        self.columns = columns
-        self.has_header = has_header
-        self.source = []
-
-    def bind(self, source):
-        """Bind a data source to this loader."""
-        self.source = source
-        return self
-
-    def _iter(self):
-        """Yield instances returned by the factory.
-
-        The factory is used to generate instances using the values of the
-        selected column for each line in the ``CSV`` file.
-
-        """
-        source = self.source
-        content = csv.reader(source)
-        if self.has_header:
-            content.next()
-        for line in content:
-            params = [line[column] for column in self.columns]
-            yield self.factory(*params)
-
-    def __iter__(self):
-        return self._iter()
+    content = csv.reader(source)
+    if has_header:
+        content.next()
+    for line in content:
+        params = [line[column] for column in columns]
+        value = factory(*params)
+        if value is not None:
+            yield value
 
 
-class ChunkedLoader(object):
+def chunked_loader(ordered_iter1, ordered_iter2, chunk_hint=16384):
     """A loading strategy for running large imports as multiple smaller ones.
 
-    The main functionality of this loader is to create and populate in parallel
-    two different ``DataSet`` instances with at most ``chunk_hint`` elements
-    from two ordered generators always with the smaller values:
+    The main functionality of this loader is to split two order iterators in
+    smaller lists while keeping the ordering and their combined length around
+    ``chunk_hint`` size.
 
-    >>> cl = ChunkedLoader(set, set, 5)
-    >>> loader = cl.loader([10, 20, 30, 40], [11, 12, 50, 60])
+    >>> loader = chunked_loader([10, 20, 30, 40], [11, 12, 50, 60], 5)
     >>> source, destination = loader.next()
     >>> sorted(source)
     [10, 20, 30]
@@ -190,7 +104,7 @@ class ChunkedLoader(object):
     The algorithm includes the next element in the chunk if it's equal to the
     last one already included:
 
-    >>> loader = cl.loader([10, 20, 30, 40], [11, 12, 30, 60])
+    >>> loader = chunked_loader([10, 20, 30, 40], [11, 12, 30, 60], 5)
     >>> source, destination = loader.next()
     >>> sorted(source)
     [10, 20, 30]
@@ -200,7 +114,7 @@ class ChunkedLoader(object):
     In case the number of elements its divisible by the chunk size, the results
     should still be correct:
 
-    >>> loader = cl.loader([10, 20, 30, 40, 50], [11, 12, 13, 60, 70])
+    >>> loader = chunked_loader([10, 20, 30, 40, 50], [11, 12, 13, 60, 70], 5)
     >>> source, destination = loader.next()
     >>> sorted(source)
     [10, 20]
@@ -219,7 +133,7 @@ class ChunkedLoader(object):
     If one of the loader is empty, the algorithm should still function
     correctly:
 
-    >>> loader = cl.loader([1, 2, 3, 4, 5, 6], [])
+    >>> loader = chunked_loader([1, 2, 3, 4, 5, 6], [], 5)
     >>> source, destination = loader.next()
     >>> sorted(source)
     [1, 2, 3, 4, 5]
@@ -230,7 +144,7 @@ class ChunkedLoader(object):
     [6]
     >>> sorted(destination)
     []
-    >>> loader = cl.loader([], [1, 2, 3, 4, 5, 6])
+    >>> loader = chunked_loader([], [1, 2, 3, 4, 5, 6], 5)
     >>> source, destination = loader.next()
     >>> sorted(source)
     []
@@ -243,53 +157,51 @@ class ChunkedLoader(object):
     [6]
 
     """
-    def __init__(self, source_factory, destination_factory, chunk_hint=16384):
-        self.source_factory = source_factory
-        self.destination_factory = destination_factory
-        self.chunk_hint = chunk_hint
-
-    def loader(self, source_loader, destination_loader):
-        """
-        A generator that can be used to load source and destination instances
-        of :py:class:`~.importtools.datasets.DataSet`
-
-        This generator yield source and destination tulpes.
-
-        """
-        w_source = self.dataset_wrapper(source_loader, True)
-        w_destination = self.dataset_wrapper(destination_loader, False)
-        merged = heapq.merge(w_source, w_destination)
-
-        while True:
-            source = self.source_factory()
-            destination = self.destination_factory()
-            current_chunk = itertools.islice(merged, self.chunk_hint)
-            empty = True
-            for importable, from_source in current_chunk:
-                empty = False
-                if from_source:
-                    source.add(importable)
-                else:
-                    destination.add(importable)
-            if empty:
-                raise StopIteration
-
-            try:
-                next_importable, next_from_source = merged.next()
-            except StopIteration:
-                pass
+    i1 = _iter_const(ordered_iter1, True)
+    i2 = _iter_const(ordered_iter2, False)
+    merged = heapq.merge(i1, i2)
+    eq = lambda x, y: x[0] == y[0]
+    chunk, iterator = _get_chunk(merged, chunk_hint, eq)
+    while chunk:
+        i1_elemens = list()
+        i2_elemens = list()
+        for element, from_iter1 in chunk:
+            if from_iter1:
+                i1_elemens.append(element)
             else:
-                if next_importable == importable:
-                    if next_from_source:
-                        source.add(next_importable)
-                    else:
-                        destination.add(next_importable)
-                else:
-                    v = (next_importable, next_from_source)
-                    merged = itertools.chain([v], merged)
+                i2_elemens.append(element)
+        del chunk
+        yield i1_elemens, i2_elemens
+        chunk, iterator = _get_chunk(iterator, chunk_hint, eq)
 
-            yield source, destination
 
-    def dataset_wrapper(self, g, const):
-        for value in g:
-            yield value, const
+def _iter_const(g, const):
+    for value in g:
+        yield value, const
+
+def _get_chunk(iterator, chunk_hint, eq=lambda x, y: x == y):
+    """
+    Return a ``list`` of chunk_hint size from the iterator. Once the ``list``
+    size is equal to ``chunk_hint`` it continues to add elements while they are
+    equal to the last one.
+
+    """
+    result = []
+
+    try:
+        # Test if the iterator is empty
+        iterator = itertools.chain([iterator.next()], iterator)
+    except StopIteration:
+        return result, iterator
+
+    current_chunk = itertools.islice(iterator, chunk_hint)
+    for element in current_chunk:
+        result.append(element)
+    for next_element in iterator:
+        if eq(next_element, element):
+            result.append(next_element)
+        else:
+            iterator = itertools.chain([next_element], iterator)
+            break
+
+    return result, iterator
